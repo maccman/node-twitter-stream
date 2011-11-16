2739f11965c70de33d65f97b5f105cfd91a09768
Raymond <raymond@lmws.net>
2011-11-16 13:50:11 -0800

    make action configurable. add error handling in case required option is missing

--- test-twitter-stream.js
+++ test-twitter-stream.js
@@ -1,11 +1,12 @@
 var ts = require('./twitter-stream.js');
 
-
 require('fs').readFile('test-config.json', 'utf8', function(error, config_file_data) {
-	var stream = ts.connect(JSON.parse(config_file_data));
-	stream.on('status', function(status) {
-		console.info(status);
-	});
+  var config = JSON.parse(config_file_data);
+  config.action = 'sample';
+  var stream = ts.connect(config);
+  stream.on('status', function(status) {
+    console.info(status);
+  });
 })
 
 
--- twitter-stream.js
+++ twitter-stream.js
@@ -2,10 +2,14 @@ var https = require('https');
 var events = require('events');
 
 exports.connect = function(options) {
+  [ 'action', 'screen_name', 'password'].forEach(function(requiredOption) {
+	  if (options[requiredOption] === undefined) throw requiredOption + " is missing";
+	});
+
   var emitter = new events.EventEmitter;
   var request = https.request({
     host: 'stream.twitter.com',
-    path: '/1/statuses/sample.json',
+    path: '/1/statuses/' + options.action + '.json',
     auth: options.screen_name + ':' + options.password,
   }, function(response) {
     response.setEncoding('utf8');
